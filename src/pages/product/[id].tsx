3c4b5227a389b82b9bf127ca5ced2b05502169fe
daniellima <Daniel.Silva.ext@cyrela.com.br>
2023-05-01 21:37:59 -0300

    feat: config the routes of app

---
+++ src/pages/product/[id].tsx
@@ -0,0 +1,19 @@
+import Head from "next/head";
+import { useRouter } from "next/router";
+
+export default function Product() {
+  const { query } = useRouter();
+  return (
+    <>
+      <Head>
+        <title>Create Next App</title>
+        <meta name="description" content="📚Book store" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main>
+        <h1>Product: {JSON.stringify(query)}</h1>
+      </main>
+    </>
+  );
+}
