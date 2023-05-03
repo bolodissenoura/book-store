f18967973594a0719935da51a5001d50d75f6adb
daniellima <Daniel.Silva.ext@cyrela.com.br>
2023-05-03 18:45:50 -0300

    feat: styles from carousel slider

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,18 +1,28 @@
 import Head from "next/head";
 import { styled } from "../styles";
+import { HomeContainer, Product } from "../styles/pages/home";
+import Image from "next/image";
+import book1 from "../assets/exbook1.png";
 
-const Button = styled("button", {
-  backgroundColor: "$primary50",
-  height: "50px",
-  padding: "0.5rem",
-  border: "none",
-  borderRadius: "0.25rem",
-  "&:hover": {
-    cursor: "pointer",
-  },
-});
+import { useKeenSlider } from "keen-slider/react";
 
+import "keen-slider/keen-slider.min.css";
 export default function Home() {
+  const [sliderRef] = useKeenSlider({
+    slides: {
+      perView: 1,
+      spacing: 32,
+    },
+    breakpoints: {
+      "(min-width: 800px)": {
+        slides: {
+          perView: 2,
+          spacing: 48,
+        },
+      },
+    },
+  });
+
   return (
     <>
       <Head>
@@ -21,10 +31,36 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main>
-        <h1>Hello</h1>
-        <Button>KOASOKPAPOKS</Button>
-      </main>
+      <HomeContainer ref={sliderRef} className="keen-slider">
+        <Product className="keen-slider__slide">
+          <Image src={book1} width={300} height={480} alt="" />
+          <footer>
+            <strong>Book x</strong>
+            <span>79,90</span>
+          </footer>
+        </Product>
+        <Product className="keen-slider__slide">
+          <Image src={book1} width={300} height={480} alt="" />
+          <footer>
+            <strong>Book x</strong>
+            <span>79,90</span>
+          </footer>
+        </Product>
+        <Product className="keen-slider__slide">
+          <Image src={book1} width={300} height={480} alt="" />
+          <footer>
+            <strong>Book x</strong>
+            <span>79,90</span>
+          </footer>
+        </Product>
+        <Product className="keen-slider__slide">
+          <Image src={book1} width={300} height={480} alt="" />
+          <footer>
+            <strong>Book x</strong>
+            <span>79,90</span>
+          </footer>
+        </Product>
+      </HomeContainer>
     </>
   );
 }
